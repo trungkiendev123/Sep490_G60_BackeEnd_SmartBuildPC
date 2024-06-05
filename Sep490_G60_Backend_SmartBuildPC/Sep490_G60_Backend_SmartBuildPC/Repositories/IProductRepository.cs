9c14953543260ce0f59dd476cd0c56df0f5d90cb
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-06-05 21:27:50 +0700

    Product repository

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Sep490_G60_Backend_SmartBuildPC.DTOs;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<ProductDTO> GetProduct(int id);
+        
+    }
+}
---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sep490_G60_Backend_SmartBuildPC.DTOs;
+using Sep490_G60_Backend_SmartBuildPC.Models;
+using Sep490_G60_Backend_SmartBuildPC.Repositories;
+using System.Collections.Generic;
+
+namespace Sep490_G60_SmartBuildPC_BE.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+
+        private readonly SMARTPCContext _context;
+        public ProductRepository(SMARTPCContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<ProductDTO> GetProduct(int id)
+        {
+            try
+            {
+                var list = await _context.Products.Include(x => x.Category).Where(x => x.ProductId == id).Select(n => new ProductDTO { ProductId = n.ProductId,ProductName = n.ProductName }).SingleAsync() ;
+                return list;
+                //return list;
+            }
+            catch (Exception ex)
+            {
+                
+                throw new Exception("An error occurred while getting the product.", ex);
+            }
+        }
+
+
+        
+
+    }
+}
