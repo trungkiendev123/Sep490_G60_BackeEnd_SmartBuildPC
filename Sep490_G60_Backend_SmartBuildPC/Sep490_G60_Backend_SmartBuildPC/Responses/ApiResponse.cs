a56cb64514ec3b489befec1650a123356dcf6e2c
trungkiendev123 <hothithaonguyen832002@gmail.com>
2024-06-05 11:35:09 +0700

    initialzie project

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/ApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Responses
+{
+    public class ApiResponse
+    {
+        public ApiResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+        public object Result { get; set; }
+
+        public String Message { get; set; }
+    }
+}
