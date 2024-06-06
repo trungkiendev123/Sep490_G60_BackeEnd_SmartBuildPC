6f2c6ae7f5b9ccee4f54d5a81548263099915272
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-06-06 17:02:21 +0700

    Update ProductRepository.cs

--- Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/ProductRepository.cs
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/ProductRepository.cs
@@ -31,6 +31,59 @@ namespace Sep490_G60_SmartBuildPC_BE.Repositories
         }
 
 
+        public async Task<List<ProductDTO>> GetProductByBrand(string brandName)
+{
+    try
+    {
+        var products = await _context.Products
+            .Where(x => x.Brand == brandName)
+            .Select(n => new ProductDTO
+            {
+                ProductId = n.ProductId,
+                ProductName = n.ProductName
+            })
+            .ToListAsync();
+
+        return products;
+    }
+    catch (Exception ex)
+    {
+        throw new Exception("An error occurred while getting the products by brand.", ex);
+    }
+}
+
+
+
+
+
+public async Task<List<ProductDTO>> GetProductByGroup(string name)
+{
+    try
+    {
+        var products = await (from g in _context.Groups
+                              join pb in _context.PcbuildParts on g.PcbuildId equals pb.PcbuildId
+                              join p in _context.Products on pb.PartId equals p.ProductId
+                              where g.Pctype == name
+                              select new ProductDTO
+                              {
+                                  ProductId = p.ProductId,
+
+                                  ProductName = p.ProductName,
+                                  Description = p.Description,
+                                  Price = p.Price,
+                                  Warranty = p.Warranty,
+                                  Brand = p.Brand
+                              }).ToListAsync();
+
+        return products;
+    }
+    catch (Exception ex)
+    {
+        throw new Exception("An error occurred while getting the products by group.", ex);
+    }
+}
+
+
         
 
     }
