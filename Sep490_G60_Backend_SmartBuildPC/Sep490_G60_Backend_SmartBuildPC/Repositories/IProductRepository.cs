63c2dcd40a5f807ba9ce1babf26c34901e309b42
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-06-15 22:42:05 +0700

    Update IProductRepository.cs

--- Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/IProductRepository.cs
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/IProductRepository.cs
@@ -4,19 +4,20 @@ using Sep490_G60_Backend_SmartBuildPC.Responses;
 namespace Sep490_G60_Backend_SmartBuildPC.Repositories
 {
     public interface IProductRepository
-    {
-        Task<ProductDTO> GetProduct(int id);
+{
+    Task<ProductDTO> GetProduct(int id);
 
-        Task<List<ProductDTO>> GetProductByBrand(string brandName);
-        
-        Task<List<ProductDTO>> GetProductByGroup(string name);
-        Task<List<ProductDTO>> GetAllProducts(int pageNumber, int pageSize);
-        
+    Task<List<ProductDTO>> GetProductByBrand(string brandName);
+    
+    Task<List<ProductDTO>> GetProductByGroup(string name);
+    Task<List<ProductDTO>> GetAllProducts(int pageNumber, int pageSize);
+    
 
-        Task<IEnumerable<ProductDTO>> GetProductsByCategory(int categoryID);
-        Task<List<ProductDTO>> GetProductsByKeyword(string keyword, int pageNumber = 1, int pageSize = 50);
+    Task<IEnumerable<ProductDTO>> GetProductsByCategory(int categoryID);
+    Task<List<ProductDTO>> GetProductsByKeyword(string keyword, int pageNumber = 1, int pageSize = 50);
 
-        Task<PreviewProductDTO> PreviewProduct(int id);
 
-    }
+    Task<PreviewProductDTO> PreviewProduct(int id);
+
+}
 }
