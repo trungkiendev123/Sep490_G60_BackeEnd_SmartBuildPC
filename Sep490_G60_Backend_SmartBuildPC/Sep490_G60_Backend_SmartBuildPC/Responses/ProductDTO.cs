312aa6378d46be9f256aca6eb61a5c56c65c39c1
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-06-15 22:40:05 +0700

    Update ProductDTO.cs

--- Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/ProductDTO.cs
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/ProductDTO.cs
@@ -1,13 +1,18 @@
 ﻿namespace Sep490_G60_Backend_SmartBuildPC.DTOs
 {
     public class ProductDTO
-    {
-        public int ProductId { get; set; }
-        public string CategoryName { get; set; }
-        public string ProductName { get; set; } = null!;
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string Warranty { get; set; }
-        public string Brand { get; set; }
-    }
+{
+    public int ProductId { get; set; }
+    public string CategoryName { get; set; }
+    public string ProductName { get; set; } = null!;
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Warranty { get; set; }
+    public string Brand { get; set; }
+
+    public string Tag { get; set; }
+    public int TDP { get; set; }
+    public string? ImageLink { get; set; }
+    public List<string> StoreNames { get; set; }
+}
 }
