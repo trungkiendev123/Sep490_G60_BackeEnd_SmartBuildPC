5b4add5445a361993b0b26432502b34e3db1e3a4
trungkiendev123 <hothithaonguyen832002@gmail.com>
2024-06-17 20:57:43 +0700

    cart repo

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/CartRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sep490_G60_Backend_SmartBuildPC.Models;
+using Sep490_G60_Backend_SmartBuildPC.Requests;
+using Sep490_G60_Backend_SmartBuildPC.Service;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Repositories
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly SMARTPCContext _context;
+        public CartRepository(SMARTPCContext context)
+        {
+            _context = context;
+        }
+        public async void AddCart(string email,AddCartRequest request)
+        {
+            try
+            {
+                var account = await _context.Accounts.FirstOrDefaultAsync(x => x.Email.Equals(email));
+                var customer = await _context.Customers.FirstOrDefaultAsync(x => x.AccountId.Equals(account.AccountId));
+                var cartUser = await _context.Carts.FirstOrDefaultAsync(x => x.CustomerId.Equals(customer.CustomerId) && x.ProductId == request.ProductID);
+                if(cartUser == null)
+                {
+                    Cart cart = new Cart()
+                    {
+                        CustomerId = customer.CustomerId,
+                        ProductId = request.ProductID,
+                        Quantity = request.Quantity
+                    };
+                    _context.Carts.Add(cart);
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    cartUser.Quantity += request.Quantity;
+                    _context.Carts.Update(cartUser);
+                    _context.SaveChanges();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while do this action", ex);
+            }
+        }
+    }
+}
---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Validation/ValidateCart.cs
@@ -0,0 +1,36 @@
+﻿using Sep490_G60_Backend_SmartBuildPC.Models;
+using Sep490_G60_Backend_SmartBuildPC.Requests;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Validation
+{
+    public class ValidateCart
+    {
+        private readonly SMARTPCContext _context;
+        public ValidateCart(SMARTPCContext context)
+        {
+            _context = context;
+        }
+        public List<string> validateCart(AddCartRequest request)
+        {
+            List<string> errors = new List<string>();
+            try
+            {
+                if(request.Quantity < 0)
+                {
+                    errors.Add("Quantity must > 0");
+                }
+                var ids = _context.Products.ToList().Select(x => x.ProductId);
+                if (!ids.Contains(request.ProductID))
+                {
+                    errors.Add("Product ID not found");
+                }
+
+            }
+            catch (Exception e)
+            {
+                errors.Add(e.Message);
+            }
+            return errors;
+        }
+    }
+}
