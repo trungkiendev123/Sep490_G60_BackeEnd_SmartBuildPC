ec8780683f3d8e83500425ca5dd6890b2ee79b1c
trungkiendev123 <hothithaonguyen832002@gmail.com>
2024-06-17 21:01:00 +0700

    add cart api

--- Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Controllers/CartController.cs
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Controllers/CartController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Sep490_G60_Backend_SmartBuildPC.Repositories;
+using Sep490_G60_Backend_SmartBuildPC.Requests;
+using Sep490_G60_Backend_SmartBuildPC.Responses;
+using Sep490_G60_Backend_SmartBuildPC.Service;
+using Sep490_G60_Backend_SmartBuildPC.Validation;
+using System.Data;
+using System.Net;
 
 namespace Sep490_G60_Backend_SmartBuildPC.Controllers
 {
@@ -7,5 +15,51 @@ namespace Sep490_G60_Backend_SmartBuildPC.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly ValidateCart _validate;
+
+        private readonly ICartRepository _repository;
+
+
+        public CartController( ValidateCart validate, ICartRepository repository)
+        {
+            _validate = validate;
+            _repository = repository;
+        }
+        [HttpPost("AddAccount")]
+        [Authorize(Roles = "CUSTOMER")]
+        public async Task<ActionResult<ApiResponse>> AddCart(AddCartRequest request)
+        {
+            var _response = new ApiResponse();
+            List<string> errors = _validate.validateCart(request);
+            try
+            {
+                if (errors.Count > 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.IsSuccess = false;
+                    _response.Message = "Add cart fail";
+                    _response.ErrorMessages = errors;
+                }
+                else
+                {
+                    _repository.AddCart(request);
+                    _response.StatusCode = HttpStatusCode.Created;
+                    _response.IsSuccess = true;
+                    _response.Message = "Add cart success";
+                }
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse
+                {
+                    StatusCode = HttpStatusCode.InternalServerError,
+                    IsSuccess = false,
+                    Message = "Add cart fail",
+                    ErrorMessages = new List<string> { "Something error from the server" }
+                };
+            }
+
+        }
     }
 }
