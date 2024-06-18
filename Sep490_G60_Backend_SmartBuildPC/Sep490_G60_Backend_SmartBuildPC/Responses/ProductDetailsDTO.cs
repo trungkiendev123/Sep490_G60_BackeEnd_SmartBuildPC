7f41783a786d4842a3eff5e96a190c9c3fe45a16
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-06-18 23:45:38 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/ProductDetailsDTO.cs
@@ -0,0 +1,10 @@
+﻿using Sep490_G60_Backend_SmartBuildPC.DTOs;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Responses
+{
+    public class ProductDetailsDTO
+    {
+        public ProductDTO Product { get; set; }
+        public List<ProductDTO> SimilarPriceProducts { get; set; }
+    }
+}
