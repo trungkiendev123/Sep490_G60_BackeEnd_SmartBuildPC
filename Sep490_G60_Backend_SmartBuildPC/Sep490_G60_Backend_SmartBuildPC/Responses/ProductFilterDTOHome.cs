bbb0f0b2d7be83cab4458fed6468c2b0ddf7d55f
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-06-24 23:37:21 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/ProductFilterDTOHome.cs
@@ -0,0 +1,12 @@
+﻿namespace Sep490_G60_Backend_SmartBuildPC.Responses
+{
+    public class ProductFilterDTOHome
+    {
+
+        public string StoreName { get; set; }
+        public decimal? PriceFrom { get; set; }
+        public decimal? PriceTo { get; set; }
+        
+        public string Category { get; set; }
+    }
+}
