64b67611836b67102c8e8268f960ee14e6e20a64
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-06-24 23:32:00 +0700

    Update CreateProductDTO.cs

--- Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/CreateProductDTO.cs
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/CreateProductDTO.cs
@@ -1,16 +1,22 @@
 ﻿namespace Sep490_G60_Backend_SmartBuildPC.Responses
 {
     public class CreateProductDTO
-    {
-        public int CategoryID { get; set; }
-        public string ProductName { get; set; } = null!;
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string Warranty { get; set; }
-        public string Brand { get; set; }
-        public string Tag { get; set; }
-        public int TDP { get; set; }
-        public string ImageLink { get; set; }
+{
+    public int CategoryID { get; set; }  
+    public string ProductName { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string Warranty { get; set; }
+    public string Brand { get; set; }
+    public string Tag { get; set; }
+    public int TDP { get; set; }
+    public string ImageLink { get; set; }
+    public List<StoreStockDTO> StoreStocks { get; set; }
+}
 
-    }
+public class StoreStockDTO
+{
+    public int StoreID { get; set; } 
+    public int StockQuantity { get; set; }
+}
 }
