8e7adc56b040c164fa80c7f272a47bbdaa6236fe
unknown <hothithaonguyen832002@gmail.com>
2024-06-24 11:05:31 +0700

    add cart repo

--- Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/CartRepository.cs
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/CartRepository.cs
@@ -12,7 +12,7 @@ namespace Sep490_G60_Backend_SmartBuildPC.Repositories
         {
             _context = context;
         }
-        public void AddCart(string email,AddCartRequest request)
+        public void AddCart(string email,ChangeCartRequest request)
         {
             try
             {
@@ -32,6 +32,36 @@ namespace Sep490_G60_Backend_SmartBuildPC.Repositories
                 else
                 {
                     cartUser.Quantity += request.Quantity;
+                    _context.Carts.Update(cartUser);
+                }
+                _context.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while do this action", ex);
+            }
+        }
+        public void UpdateCart(string email, ChangeCartRequest request)
+        {
+            try
+            {
+                var account = _context.Accounts.FirstOrDefault(x => x.Email.Equals(email));
+                var customer = _context.Customers.FirstOrDefault(x => x.AccountId.Equals(account.AccountId));
+                var cartUser = _context.Carts.FirstOrDefault(x => x.CustomerId.ToString().ToUpper().Equals(customer.CustomerId.ToString().ToUpper()) && x.ProductId == request.ProductID);
+                if (cartUser == null)
+                {
+                    Cart cart = new Cart()
+                    {
+                        CustomerId = customer.CustomerId,
+                        ProductId = request.ProductID,
+                        Quantity = request.Quantity
+                    };
+                    _context.Carts.Add(cart);
+                }
+                else
+                {
+                    cartUser.Quantity = request.Quantity;
                     _context.Carts.Update(cartUser);
                 }
                 _context.SaveChanges();
