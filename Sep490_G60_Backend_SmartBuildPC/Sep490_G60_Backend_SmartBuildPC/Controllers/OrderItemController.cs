147e3f0b32d70b3908457a7d2939a06574bc3f4a
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-10 03:08:43 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Controllers/OrderItemController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sep490_G60_Backend_SmartBuildPC.Repositories;
+using Sep490_G60_Backend_SmartBuildPC.Responses;
+using System.Net;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderItemController : ControllerBase
+    {
+
+
+        private readonly IOrderItemRepository repository;
+        public OrderItemController(IOrderItemRepository _repository)
+        {
+            repository = _repository;
+        }
+
+
+        [HttpGet("GetOrderItems")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ApiResponse>> GetOrderItems(int orderId)
+        {
+            var response = new ApiResponse();
+            try
+            {
+                var orderItems = await repository.GetOrderItems(orderId);
+
+                if (orderItems == null || orderItems.Count == 0)
+                {
+                    response.IsSuccess = false;
+                    response.ErrorMessages = new List<string> { "Order items not found." };
+                    return NotFound(response);
+                }
+
+                response.StatusCode = HttpStatusCode.OK;
+                response.Result = orderItems;
+                response.IsSuccess = true;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, response);
+            }
+        }
+
+
+
+        [HttpGet("GetAllOrderItems")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<List<OrderItemDTO>>> GetAllOrderItems(int page = 1, int pageSize = 10)
+        {
+            try
+            {
+                var orderItems = await repository.GetAllOrderItems(page, pageSize);
+                return Ok(orderItems);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error retrieving all order items: {ex.Message}");
+            }
+        }
+    }
+}
