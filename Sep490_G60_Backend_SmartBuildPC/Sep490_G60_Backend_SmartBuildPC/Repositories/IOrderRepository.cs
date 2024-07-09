af985552fc65c904a45cda602510b0fddd34c7f1
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-10 02:59:34 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Sep490_G60_Backend_SmartBuildPC.Responses;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task<OrderDTO> GetOrderByIdAsync(int orderId);
+        Task UpdateOrderStatusAsync(UpdateOrderStatusDTO updateOrderStatusDto);
+        Task<BillDTO> GetBillByOrderIdAsync(int orderId);
+        Task<AccountDTO> GetAccountByOrderIdAsync(int orderId);
+    }
+}
