907448f85dc5324fcb831ef714cb342cc6c8b8ff
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-10 03:00:16 +0700

    Update IOrderRepository.cs

--- Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/IOrderRepository.cs
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/IOrderRepository.cs
@@ -4,9 +4,8 @@ namespace Sep490_G60_Backend_SmartBuildPC.Repositories
 {
     public interface IOrderRepository
     {
-        Task<OrderDTO> GetOrderByIdAsync(int orderId);
+        
         Task UpdateOrderStatusAsync(UpdateOrderStatusDTO updateOrderStatusDto);
-        Task<BillDTO> GetBillByOrderIdAsync(int orderId);
-        Task<AccountDTO> GetAccountByOrderIdAsync(int orderId);
+        
     }
 }
