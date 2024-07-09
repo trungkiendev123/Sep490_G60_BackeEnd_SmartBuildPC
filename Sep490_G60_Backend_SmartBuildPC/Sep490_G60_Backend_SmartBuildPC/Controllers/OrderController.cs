b1c09f4f3178a1f22bdd7314a4005f49733b384e
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-10 03:04:54 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sep490_G60_Backend_SmartBuildPC.Repositories;
+using Sep490_G60_Backend_SmartBuildPC.Responses;
+using System.Net;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+
+        private readonly IOrderRepository _orderRepository;
+        private readonly IEmailService _emailService;
+
+        public OrderController(IOrderRepository orderRepository, IEmailService emailService)
+        {
+            _orderRepository = orderRepository;
+            _emailService = emailService;
+        }
+
+
+        
+
+
+        [HttpPost("UpdateOrderStatus")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ApiResponse>> UpdateOrderStatus([FromBody] UpdateOrderStatusDTO UpdateOrderStatusDTO)
+        {
+            var _response = new ApiResponse();
+            try
+            {
+                await _orderRepository.UpdateOrderStatusAsync(UpdateOrderStatusDTO);
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+    }
+}
