f69e597d6ed1944d6c80030b082dd5322d13e9de
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-10 02:57:33 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/UpdateOrderStatusDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Sep490_G60_Backend_SmartBuildPC.Responses
+{
+    public class UpdateOrderStatusDTO
+    {
+        public int OrderID { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
