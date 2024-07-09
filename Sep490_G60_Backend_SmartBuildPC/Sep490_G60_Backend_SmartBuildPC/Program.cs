38e4f77e5c824aea6db140220edfd99973d9edf0
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-10 03:10:56 +0700

    Update Program.cs

--- Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Program.cs
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
