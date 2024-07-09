295aa70b64a6b54d1ab2d716d28df1e6c2bb5be4
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-10 03:03:21 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sep490_G60_Backend_SmartBuildPC.Models;
+using Sep490_G60_Backend_SmartBuildPC.Responses;
+using System.Threading.Tasks;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly SMARTPCContext _context;
+
+        public OrderRepository(SMARTPCContext context)
+        {
+            _context = context;
+        }
+
+        
+
+        public async Task UpdateOrderStatusAsync(UpdateOrderStatusDTO updateOrderStatusDto)
+        {
+            var order = await _context.Orders.FirstOrDefaultAsync(o => o.OrderId == updateOrderStatusDto.OrderID);
+            if (order != null)
+            {
+                order.OrderStatus = updateOrderStatusDto.OrderStatus;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        
+        
+
+
+    }
+}
