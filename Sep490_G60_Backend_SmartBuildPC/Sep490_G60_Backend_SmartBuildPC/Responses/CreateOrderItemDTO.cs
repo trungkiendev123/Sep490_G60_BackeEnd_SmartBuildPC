f6374ef77852ec7cc077fd963bb4eb29b3e12cc5
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-12 23:55:47 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/CreateOrderItemDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Sep490_G60_Backend_SmartBuildPC.Responses
+{
+    public class CreateOrderItemDTO
+    {
+        public string Phone { get; set; }  // Thêm trường Phone vào DTO
+
+        // Các trường dữ liệu khác vẫn giữ nguyên
+        public DateTime OrderDate { get; set; }
+        public string OrderStatus { get; set; }
+        public string OrderAddress { get; set; }
+        public DateTime ReceiveDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public DateTime WarrantyStartDate { get; set; }
+        public DateTime WarrantyEndDate { get; set; }
+    }
+}
