a19553151565c1357fcabc779db19cf107b3e53b
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-12 23:37:36 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/CategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sep490_G60_Backend_SmartBuildPC.Models;
+using Sep490_G60_Backend_SmartBuildPC.Responses;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly SMARTPCContext _context;
+
+        public CategoryRepository(SMARTPCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<CategoryDTO>> GetAllCategoriesAsync()
+        {
+            var categories = await _context.Categories
+                                           .Select(c => new CategoryDTO
+                                           {
+                                               CategoryId = c.CategoryId,
+                                               CategoryName = c.CategoryName
+                                           })
+                                           .ToListAsync();
+
+            return categories;
+        }
+
+
+
+
+        public async Task<CategoryDTO> GetCategoryByIdAsync(int categoryId)
+        {
+            var category = await _context.Categories
+                                         .Where(c => c.CategoryId == categoryId)
+                                         .Select(c => new CategoryDTO
+                                         {
+                                             CategoryId = c.CategoryId,
+                                             CategoryName = c.CategoryName
+                                         })
+                                         .FirstOrDefaultAsync();
+
+            if (category == null)
+            {
+                throw new KeyNotFoundException("Category not found.");
+            }
+
+            return category;
+        }
+    }
+}
---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Sep490_G60_Backend_SmartBuildPC.Responses;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<CategoryDTO>> GetAllCategoriesAsync();
+        Task<CategoryDTO> GetCategoryByIdAsync(int categoryId);
+    }
+}
