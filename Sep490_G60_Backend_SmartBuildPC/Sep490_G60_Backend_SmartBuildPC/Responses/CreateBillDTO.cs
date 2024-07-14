aa3fd372f8303daae3f78b160985aff8eed57ffd
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-14 17:25:44 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Responses/CreateBillDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Sep490_G60_Backend_SmartBuildPC.Responses
+{
+    public class CreateBillDTO
+    {
+        public int? OrderId { get; set; }
+        public DateTime? BillDate { get; set; }
+        public int? TaxIn { get; set; }
+        public string? Address { get; set; }
+    }
+}
