c7690d33993af81e1d024a7af827bad8400ca782
hungnq2808 <124073081+hungnq2808@users.noreply.github.com>
2024-07-14 17:26:54 +0700

    Add files via upload

---
+++ Sep490_G60_Backend_SmartBuildPC/Sep490_G60_Backend_SmartBuildPC/Controllers/BillController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sep490_G60_Backend_SmartBuildPC.Repositories;
+using Sep490_G60_Backend_SmartBuildPC.Responses;
+using System.Net;
+
+namespace Sep490_G60_Backend_SmartBuildPC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BillController : ControllerBase
+    {
+        private readonly IBillRepository _billRepository;
+
+        public BillController(IBillRepository billRepository)
+        {
+            _billRepository = billRepository;
+        }
+
+        [HttpPost("CreateBill")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ApiResponse>> CreateBill([FromBody] CreateBillDTO createBillDTO)
+        {
+            var _response = new ApiResponse();
+            try
+            {
+                await _billRepository.CreateBillAsync(createBillDTO);
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (InvalidOperationException ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+                return BadRequest(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [HttpGet("GetAllBills")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<ApiResponse>> GetAllBills()
+        {
+            var _response = new ApiResponse();
+            try
+            {
+                var bills = await _billRepository.GetAllBillsAsync();
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = bills;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
